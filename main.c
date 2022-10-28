19032ac4a3a27ee4a6f72d593b78c8ceaff0b527
onurkinay <ben@onurkinay.com.tr>
2022-10-28 19:33:12 +0300

    just testing fork function

--- main.c
+++ main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int initShell()
+{
+}
+
+int runComm()
+{
+    char *Args[3];
+
+    Args[0] = "cat";
+    Args[1] = "main.c";
+    Args[2] = NULL;
+
+    int i = fork();
+
+    if (i == 0)
+    {
+        printf("Program is started to work\n");
+        if (execve("/bin/cat", Args,NULL) < 0)
+        {
+            printf("Program is failed\n");
+        }
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+        return 2;
+    }
+    return 0;
+}
+
+int exitFromShell()
+{
+    return 0;
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello world\n");
+    printf("Hello world. Test\n");
+    runComm();
     return 0;
 }
