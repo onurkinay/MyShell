35028f3eb3daf54fc713ecfd499342adbe1c0e87
onurkinay <ben@onurkinay.com.tr>
2022-10-29 19:58:55 +0300

    get filename from argv in writef

--- writef.c
+++ writef.c
@@ -21,9 +21,38 @@ char *getSystemInfo()
     printf("time:%s pid:%d ppid:%d\n",getTime(),getpid(),getppid());
 }
 
+int writeIntoFile(char *filename)
+{
+    FILE *file;
+    file = fopen(filename, "r");
+    if (file)
+    {
+        // file exists and can be opened
+        //...
+        //  close file when you're done
+        fclose(file);
+    }
+    else
+    {
+        // file doesn't exists or cannot be opened (es. you don't have access permission)
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
+
+    /*Arguman olarak gelen dosya adını alma*/
+    char* filename[100];
+    for (int i = 0; i < argc; i++)
+    {
+      if(strstr(argv[i], "-f") != NULL){
+       strcpy(filename,argv[i+1]);
+      }
+    } 
+    /*!Arguman olarak gelen dosya adını alma*/
+
+    
     getSystemInfo();
     return 0;
 }
