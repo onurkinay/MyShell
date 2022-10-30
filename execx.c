b9d6a0bf1a3598e53856172cafebdd81ae540222
onurkinay <ben@onurkinay.com.tr>
2022-10-30 20:52:11 +0300

    execx: get args

--- execx.c
+++ execx.c
@@ -3,20 +3,34 @@
 #include <sys/wait.h>
 #include <string.h>
 
+int runProgramTimes(char *program, int times)
+{
+}
+
 int main(int argc, char const *argv[])
 {
-     
+
     char howTimes[100];
     char program[100];
-    for (int i = 1; i < argc; i++)
+    char programWithArgs[100][100];
+   
+    int i = 0;
+    for (int j = 1; j < argc; j++)
     {
-        if (strstr(argv[i], "-t") != NULL)
+        if (strstr(argv[j], "-t") != NULL)
         {
-            strcpy(howTimes, argv[++i]);
+            strcpy(howTimes, argv[++j]);
         }else{
-            strcpy(program, argv[i]);
+            strcpy(programWithArgs[i], argv[j]);
+            i++;
         }
+    } 
+
+    printf("%s\n", howTimes);
+    for (int k = 0; k < i; k++)
+    {
+        printf("%s\n", programWithArgs[k]);
     }
-  
+
     return 0;
 }
